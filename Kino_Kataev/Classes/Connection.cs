d84d3ae2678580e2e98bed3d959302309a88a2b1
MaximKataev1 <maximkataev503@gmail.com>
2025-11-04 23:36:06 +0500

    Добавьте файлы проекта.

---
+++ Kino_Kataev/Classes/Connection.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino_Kataev.Classes
+{
+    public class Connection
+    {
+        private static readonly string connectionString = "server=127.0.0.1;port=3306;database=Kino;uid=root;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            if (connection != null)
+            {
+                connection.Close();
+                MySqlConnection.ClearPool(connection);
+            }
+        }
+    }
+}
