e20f4bfb83880210bc83b5415467db233972a300
MaximKataev1 <maximkataev503@gmail.com>
2025-11-05 00:26:33 +0500

    Программирование страницы кинотеатров. Катаев

--- Kino_Kataev/Models/KinoteatrModel.cs
+++ Kino_Kataev/Models/KinoteatrModel.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Kino_Kataev.Models
 {
-    public class Kinoteatr
+    public class KinoteatrModel
     {
         public int id { get; set; }
         public string name { get; set; }
         public int count_zal { get; set; }
         public int count { get; set; }
 
-        public Kinoteatr(int id, string name, int count_zal, int count) 
+        public KinoteatrModel(int id, string name, int count_zal, int count)
         {
             this.id = id;
             this.name = name;
--- Kino_Kataev/Pages/Kinoteatr.xaml.cs
+++ Kino_Kataev/Pages/Kinoteatr.xaml.cs
@@ -1,28 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Kino_Kataev.Classes;
+using Kino_Kataev.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kino_Kataev.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Kinoteatr.xaml
-    /// </summary>
     public partial class Kinoteatr : Page
     {
+        ObservableCollection<KinoteatrModel> kinoteatrs = new ObservableCollection<KinoteatrModel>();
         public Kinoteatr()
         {
             InitializeComponent();
+            SetPlaceholder(txtName, "Название");
+            SetPlaceholder(txtZal, "Кол-во залов");
+            SetPlaceholder(txtCount, "Кол-во мест");
+        }
+
+        private void SetPlaceholder(TextBox textBox, string placeholderText)
+        {
+            textBox.Tag = placeholderText;
+            textBox.Text = placeholderText;
+            textBox.Foreground = Brushes.Gray;
+            textBox.GotFocus += (sender, e) =>
+            {
+                if (textBox.Text == placeholderText)
+                {
+                    textBox.Text = "";
+                    textBox.Foreground = Brushes.Black;
+                }
+            };
+            textBox.LostFocus += (sender, e) =>
+            {
+                if (string.IsNullOrEmpty(textBox.Text))
+                {
+                    textBox.Text = placeholderText;
+                    textBox.Foreground = Brushes.Gray;
+                }
+            };
+        }
+
+        private void LoadData() 
+        {
+            kinoteatrs.Clear();
+            using (var conn = Connection.OpenConnection()) 
+            {
+                string sql = "SELECT * FROM Kinoteatr";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read()) 
+                {
+                    kinoteatrs.Add(new KinoteatrModel(
+                        reader.GetInt32("id"),
+                        reader.GetString("name"),
+                        reader.GetInt32("count_zal"),
+                        reader.GetInt32("count")
+                    ));
+                }
+            }
+            dgKinoteatr.ItemsSource = kinoteatrs;
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            using (var conn = Connection.OpenConnection())
+            {
+                string sql = "INSERT INTO Kinoteatr (name, count_zal, count) VALUES (@n, @z, @c)";
+                var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@n", txtName.Text);
+                cmd.Parameters.AddWithValue("@z", txtZal.Text);
+                cmd.Parameters.AddWithValue("@c", txtCount.Text);
+                cmd.ExecuteNonQuery();
+            }
+            LoadData();
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+            if (dgKinoteatr.SelectedItem is KinoteatrModel selected)
+            {
+                using (var conn = Connection.OpenConnection())
+                {
+                    string sql = "UPDATE Kinoteatr SET name=@n, count_zal=@z, count=@c WHERE id=@id";
+                    var cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@n", txtName.Text);
+                    cmd.Parameters.AddWithValue("@z", txtZal.Text);
+                    cmd.Parameters.AddWithValue("@c", txtCount.Text);
+                    cmd.Parameters.AddWithValue("@id", selected.id);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (dgKinoteatr.SelectedItem is KinoteatrModel selected)
+            {
+                using (var conn = Connection.OpenConnection())
+                {
+                    string sql = "DELETE FROM Kinoteatr WHERE id=@id";
+                    var cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@id", selected.id);
+                    cmd.ExecuteNonQuery();
+                }
+                LoadData();
+            }
         }
     }
 }
